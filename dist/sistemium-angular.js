f4fbf18d4348cefb2db9929d40740114ff711af5
Albert <kovalevskij.albert@gmail.com>
2016-04-12 16:36:53 +0300

    0.1.6 dist

--- dist/sistemium-angular.js
+++ dist/sistemium-angular.js
@@ -37,6 +37,32 @@
 
 })(angular);
 
+(function() {
+
+  function config(toastrConfig) {
+
+    angular.extend (toastrConfig,{
+      allowHtml: true,
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: false,
+      progressBar: false,
+      iconClasses: {
+        error: 'alert alert-danger',
+        info: 'alert alert-info',
+        success: 'alert alert-success',
+        warning: 'alert alert-warning'
+      }
+    });
+
+  }
+
+  angular
+    .module('sistemium.dependencies')
+    .config(config);
+
+})();
+
 (function () {
 
   angular.module('sistemium.directives')
