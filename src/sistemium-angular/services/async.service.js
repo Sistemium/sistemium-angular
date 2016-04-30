a3b8f89220e76c0e4e08637151e8b022c05e82c5
Alexander Levin <alevin.ru@gmail.com>
2016-04-30 10:07:56 +0300

    saAsync.chunkSerial

--- src/sistemium-angular/services/async.service.js
+++ src/sistemium-angular/services/async.service.js
@@ -2,9 +2,51 @@
 
 (function () {
 
-  function saAsync ($window) {
+  function saAsync ($window,$q) {
 
-    return $window.async;
+    var async = $window.async;
+
+    function chunkSerial (chunkSize, data, datumFn, onChunkSuccessFn, onChunkErrorFn) {
+
+      var chunks = _.chunk(data,chunkSize);
+
+      var series = _.map (chunks,function(chunk){
+
+        return function (done) {
+
+          var q = $q.all(_.map(chunk,function(datum){
+            return datumFn (datum);
+          }));
+
+          q.then (function(chunk){
+            onChunkSuccessFn (chunk);
+            done(null,chunk);
+          });
+
+          return q.catch(function (err){
+            onChunkErrorFn (chunk);
+            done(err,chunk);
+          });
+        };
+
+      });
+
+      return $q(function(resolve,reject){
+        async.series(series,function(err,results) {
+
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+
+        });
+      });
+    }
+
+    return angular.extend (async,{
+      chunkSerial: chunkSerial
+    });
 
   }
 
