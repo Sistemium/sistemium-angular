9684435ecf2cef9140b4d3b178c542182b612ce6
Alexander Levin <alevin.ru@gmail.com>
2016-12-12 10:25:55 +0200

    selectOnFocus.directive

---
+++ src/sistemium-angular/directives/selectOnFocus.directive.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+
+  function selectOnFocus($window) {
+    return {
+
+      restrict: 'A',
+
+      link: function (scope, element) {
+        element.on('click', function () {
+          if (!$window.getSelection().toString()) {
+            // Required for mobile Safari
+            this.setSelectionRange(0, this.value.length)
+          }
+        });
+      }
+
+    };
+  }
+
+  angular.module('sistemium.directives')
+    .directive('selectOnFocus', selectOnFocus);
+
+})();
